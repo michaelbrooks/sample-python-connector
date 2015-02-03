14f7dacd76aebb1ec68c0985360c85736c7108ff
Michael Brooks <mjbrooks@uw.edu>
2015-02-03 00:16:42 -0800

    auto-yes apt-get installs

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 export DEBIAN_FRONTEND=noninteractive
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -yq git redis-server libpython-dev tmux silversearcher-ag build-essential autoconf libtool pkg-config idle-python2.7
 
 # Mongo
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get install mongodb-org
+sudo apt-get update -y
+sudo apt-get install mongodb-org -y
 
 sudo apt-get install -y curl
 sudo curl https://bootstrap.pypa.io/get-pip.py > pip_install.py
